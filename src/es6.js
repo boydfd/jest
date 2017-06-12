88e9354a04ced0eb0fd53ca1713bee68df344fb1
rlin <rlin@thoughtworks.com>
2017-06-12 23:08:10 +0800

    [@boydfd] add es6 syntax support.

---
+++ src/es6.js
@@ -0,0 +1,3 @@
+export const arrowFunction = () => {
+  return 'arrowFunction'
+}
--- src/helloWorld.spec.js
+++ src/helloWorld.spec.js
@@ -1,3 +1,9 @@
+import { arrowFunction } from './es6'
+
 it('first test', () => {
   expect(true, true)
+})
+
+it('second test with es6 syntax', () => {
+  expect('arrowFunction', arrowFunction())
 })
